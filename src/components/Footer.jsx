7f94af47144b038c3bd3ed07eee657c03b82cf11
tuananhjenbe <nguyentuananh200904@gmail.com>
2025-12-20 21:12:37 +0700

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -141,7 +141,7 @@ export default function Footer() {
         <FloatingChatWidget />
       )}
 
-      {!isChatPage && (user.roleName === "Member" || user.roleName === "Guest") && (
+      {!isChatPage && (user.roleName === "Member" || user.roleName === "guest") && (
         <>
           {user.roleName !== "Admin" && (
             <div className="back-to-top">
